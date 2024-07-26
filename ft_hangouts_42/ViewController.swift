674241fec959d8c3b749ccc8755d09c6d6a44c9e
Carlos Rocha <carlos.rocha01@gmail.com>
2024-07-26 15:45:43 -0300

    Initial Commit

---
+++ ft_hangouts_42/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ft_hangouts_42
+//
+//  Created by Carlos Rocha on 26/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
